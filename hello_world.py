fec280768665a45aa12a0de7de171a903db3d579
‏‪michael williams <michael.williams.my@gmail.com>
2025-11-12 01:32:27 +0330

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Hallo Welt - Hello World in German
+برنامه سلام دنیا به آلمانی
+"""
+
+def main():
+    """Main function to print greeting in German"""
+    greeting = "Hallo Welt! 👋"
+    print(greeting)
+    print("=" * 30)
+    print("Willkommen! (خوش آمدید!)")
+    print("=" * 30)
+
+if __name__ == "__main__":
+    main()
